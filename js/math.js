e008c8caff95e14bffdce260ba7b42e738500496
Christopher Chantres <chris.chantres@gmail.com>
2021-09-06 09:28:29 -0500

    The canvas board is ready, mobile version is left

--- js/math.js
+++ js/math.js
@@ -14,3 +14,104 @@ document.querySelector('#calculate').addEventListener('click', () =>{
     }
     document.querySelector('#answer').innerHTML = r;
 });
+
+// ---------------------- Pizarra --------------------
+
+let miCanvas = document.querySelector('#pizarra');
+let lineas = [];
+let correccionX = 0;
+let correccionY = 0;
+let pintarLinea = false;
+// Marca el nuevo punto
+let nuevaPosicionX = 0;
+let nuevaPosicionY = 0;
+
+let posicion = miCanvas.getBoundingClientRect()
+correccionX = posicion.x;
+correccionY = posicion.y;
+
+miCanvas.width = 600;
+miCanvas.height = 270;
+
+/**
+ * Funcion que empieza a dibujar la linea
+ */
+function empezarDibujo() {
+    pintarLinea = true;
+    lineas.push([]);
+};
+
+/**
+ * Funcion que guarda la posicion de la nueva línea
+ */
+function guardarLinea() {
+    lineas[lineas.length - 1].push({
+        x: nuevaPosicionX,
+        y: nuevaPosicionY
+    });
+}
+
+// Agregar un boton para darle color
+
+/**
+ * Funcion dibuja la linea
+ */
+function dibujarLinea(event) {
+    event.preventDefault();
+    if (pintarLinea) {
+        let ctx = miCanvas.getContext('2d')
+        // Estilos de linea
+        ctx.lineJoin = ctx.lineCap = 'round';
+        ctx.lineWidth = 3;
+        // Color de la linea
+        ctx.strokeStyle = '#00E6DD';
+        // Marca el nuevo punto
+        if (event.changedTouches == undefined) {
+            // Versión ratón
+            nuevaPosicionX = event.layerX;
+            nuevaPosicionY = event.layerY;
+        } else {
+            // Versión touch, pantalla tactil
+            nuevaPosicionX = event.changedTouches[0].pageX - correccionX;
+            nuevaPosicionY = event.changedTouches[0].pageY - correccionY;
+        }
+        // Guarda la linea
+        guardarLinea();
+        // Redibuja todas las lineas guardadas
+        ctx.beginPath();
+        lineas.forEach(function (segmento) {
+            ctx.moveTo(segmento[0].x, segmento[0].y);
+            segmento.forEach(function (punto, index) {
+                ctx.lineTo(punto.x, punto.y);
+            });
+        });
+        ctx.stroke();
+    }
+}
+
+/**
+ * Funcion que deja de dibujar la linea
+ */
+function pararDibujar () {
+    pintarLinea = false;
+    guardarLinea();
+}
+
+//======================================================================
+// EVENTOS
+//======================================================================
+
+// Eventos raton
+miCanvas.addEventListener('mousedown', empezarDibujo, false);
+miCanvas.addEventListener('mousemove', dibujarLinea, false);
+miCanvas.addEventListener('mouseup', pararDibujar, false);
+
+// Eventos pantallas táctiles
+miCanvas.addEventListener('touchstart', empezarDibujo, false);
+miCanvas.addEventListener('touchmove', dibujarLinea, false);
+
+// Alert al recargar la pagina
+
+window.onbeforeunload = function() {
+    return "¿Desea recargar la página web?";
+  };
